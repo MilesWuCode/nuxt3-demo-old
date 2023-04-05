3bd0e1db5c829ed2054c737c7ac95a131c6a10a6
Miles Wu <slion0725@gmail.com>
2023-04-05 22:08:24 +0800

    upd

--- error.vue
+++ error.vue
@@ -5,9 +5,16 @@ defineProps<{
     message: string
   }
 }>()
+
+// 使用useError
+// const error = useError()
+// if (error.value.statusCode === 404) {
+//   error.value.message = 'Oops! Page not found 😔'
+// }
 </script>
 
 <template>
   <h1 class="m-2 text-3xl">{{ error.statusCode }}</h1>
   <p>{{ error.message }}</p>
+  <button @click="() => clearError({ redirect: '/' })">clearError</button>
 </template>
--- pages/on-error-captured.vue
+++ pages/on-error-captured.vue
@@ -5,14 +5,49 @@ const message = ref('')
 
 onErrorCaptured((err) => {
   message.value = err.message
+
   return false
+
+  // 或
+
+  // 錯誤換頁到400
+  // throw createError({
+  //   statusCode: 400,
+  //   statusMessage: 'Bad Request',
+  //   fatal: true,
+  // })
 })
+
+const errorLog = (err: Error) => {
+  console.log('1', err)
+}
 </script>
 
 <template>
   <div>
     <h1 class="m-2 text-3xl">onErrorCaptured</h1>
-    <ErrorButton />
-    <p v-if="message.length > 0">Error:{{ message }}</p>
+    <div class="flex flex-col space-y-4">
+      <div>
+        <p>Case 1 onErrorCaptured</p>
+
+        <ErrorButton />
+
+        <p v-if="message.length > 0">Error:{{ message }}</p>
+      </div>
+
+      <div>
+        <p>Case 2 NuxtErrorBoundary / clearError</p>
+
+        <NuxtErrorBoundary @error="errorLog">
+          <ErrorButton />
+          <!-- You use the default slot to render your content -->
+          <template #error="{ error }">
+            You can display the error locally here.
+            {{ error }}
+            <button @click="() => clearError()">Clear</button>
+          </template>
+        </NuxtErrorBoundary>
+      </div>
+    </div>
   </div>
 </template>
