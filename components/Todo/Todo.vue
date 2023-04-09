55ac1b1c9ce5f4962a714da1743741e8e1aace38
Miles Wu <slion0725@gmail.com>
2023-04-09 12:48:40 +0800

    upd

--- components/Todo/Todo.vue
+++ components/Todo/Todo.vue
@@ -5,10 +5,35 @@ const store = useTodoStore()
 </script>
 
 <template>
-  <span>{{ store.completedTotal }} / {{ store.total }}</span>
-  <ul class="menu hidden w-56 md:block">
-    <li v-for="item of store.list" :key="item.id">
-      {{ item.content }}
-    </li>
-  </ul>
+  <div class="flex flex-col space-y-2">
+    <!-- input -->
+    <div class="form-control">
+      <div class="input-group">
+        <input
+          type="text"
+          placeholder="Search…"
+          class="input-bordered input w-full"
+        />
+        <button class="btn-square btn">add</button>
+      </div>
+    </div>
+
+    <!-- filter -->
+    <div class="tabs justify-around">
+      <span class="tab">{{ store.completedTotal }} / {{ store.total }}</span>
+      <button class="tab tab-active">All</button>
+      <button class="tab">Active</button>
+      <button class="tab">Completed</button>
+    </div>
+
+    <!-- list -->
+    <ul class="menu">
+      <li v-for="item of store.list" :key="item.id" class="p-3">
+        <label class="label justify-start">
+          <input type="checkbox" class="checkbox" />
+          <span class="label-text">Remember me</span>
+        </label>
+      </li>
+    </ul>
+  </div>
 </template>
