19f3f3f34c5f5866177525a6ee298bf2c59a7a82
Miles Wu <slion0725@gmail.com>
2023-06-28 19:26:54 +0800

    upd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devtools: { enabled: true },
+
   app: {
     // Transition全域設定
     // 不換layout只換頁
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,16 @@
 import type { Config } from 'tailwindcss'
+// import defaultTheme from 'tailwindcss/defaultTheme'
 import daisyui from 'daisyui'
 
 export default {
   darkMode: 'class',
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      // colors: {
+      //   primary: defaultTheme.colors.green,
+      // },
+    },
   },
   plugins: [daisyui],
   daisyui: {
